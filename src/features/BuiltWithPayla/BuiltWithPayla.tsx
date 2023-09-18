99bfddb39823c1257d9eba27203e145e0796d79f
Ilya Daraseliya <idarase@gmail.com>
2023-09-18 18:33:20 +0200

    fix built with message

--- src/features/BuiltWithPayla/BuiltWithPayla.tsx
+++ src/features/BuiltWithPayla/BuiltWithPayla.tsx
@@ -1,11 +1,10 @@
-import {logoFont} from "@/lib/fonts"
 import {cn, getBaseUrl} from "@/lib/utils"
 
 export function BuiltWithPayla() {
   return (
     <div className="text-base text-muted-foreground text-center">
       Built with{" "}
-      <a href={getBaseUrl()} className={cn("underline", logoFont.className)}>
+      <a href={getBaseUrl()} className={cn("underline font-bold")}>
         Payla
       </a>
     </div>
