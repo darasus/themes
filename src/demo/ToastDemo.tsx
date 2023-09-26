f83e439888dc3effcde26352f6d010faa9b0c816
Ilya Daraseliya <idarase@gmail.com>
2023-09-26 08:07:34 +0200

    fix demos

--- src/demo/ToastDemo.tsx
+++ src/demo/ToastDemo.tsx
@@ -2,25 +2,29 @@
 
 import {Button} from "@/components/ui/button"
 import {ToastAction} from "@/components/ui/toast"
+import {Toaster} from "@/components/ui/toaster"
 import {useToast} from "@/components/ui/use-toast"
 
 export function ToastDemo() {
   const {toast} = useToast()
 
   return (
-    <Button
-      variant="outline"
-      onClick={() => {
-        toast({
-          title: "Scheduled: Catch up ",
-          description: "Friday, February 10, 2023 at 5:57 PM",
-          action: (
-            <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
-          ),
-        })
-      }}
-    >
-      Add to calendar
-    </Button>
+    <>
+      <Toaster />
+      <Button
+        variant="outline"
+        onClick={() => {
+          toast({
+            title: "Scheduled: Catch up ",
+            description: "Friday, February 10, 2023 at 5:57 PM",
+            action: (
+              <ToastAction altText="Goto schedule to undo">Undo</ToastAction>
+            ),
+          })
+        }}
+      >
+        Add to calendar
+      </Button>
+    </>
   )
 }
